866160503c4e77af4472da38266ababcdad9524e
lucek <lukasz.odziewa@gmail.com>
2018-01-21 00:53:49 +0100

    added bookmark params builder

---
+++ app/builders/bookmark_params_builder.rb
@@ -0,0 +1,30 @@
+module BookmarkParamsBuilder
+  def self.build_bookmark_params(user, params)
+    url      = params[:url]
+    uri      = URI.parse(url)
+    path     = uri.path
+    host     = url.gsub(uri.path, "").downcase
+    site_id  = Site.find_by_url(host).try(:id)
+    errors   = []
+
+    if !site_id
+      site = user.sites.new(url: host)
+
+      if site.save
+        site_id = site.id
+      else
+        errors << site.errors.messages
+      end
+    end
+
+    {
+      bookmark: {
+        title:      params[:title],
+        url:        path,
+        shortening: params[:shortening],
+        site_id:    site_id
+      },
+      errors: errors
+    }
+  end
+end
---
+++ spec/builders/bookmark_params_builder_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+describe BookmarkParamsBuilder, type: :model do
+  describe "#build_bookmark_params" do
+    context "correct data" do
+      let(:bookmark_params) { { title: "Bookmark", url: "http://google.de/abcd", shortening: "GDE" } }
+
+      context "site exists" do
+        let(:site) { create :site, id: 999, url: "http://google.de" }
+        let(:built_params) { BookmarkParamsBuilder.build_bookmark_params(site.user, bookmark_params) }
+
+        it "should return correct bookmark params" do
+          expect(built_params[:bookmark][:title]).to eq "Bookmark"
+          expect(built_params[:bookmark][:url]).to eq "/abcd"
+          expect(built_params[:bookmark][:shortening]).to eq "GDE"
+          expect(built_params[:bookmark][:site_id]).to eq 999
+        end
+
+        it "should return params with no errors" do
+          expect(built_params[:errors].count).to eq 0
+        end
+      end
+
+      context "site doesn't exist" do
+        let(:user) { create :user }
+        let(:built_params) { BookmarkParamsBuilder.build_bookmark_params(user, bookmark_params) }
+
+        it "should return correct bookmark params" do
+          expect(built_params[:bookmark][:title]).to eq "Bookmark"
+          expect(built_params[:bookmark][:url]).to eq "/abcd"
+          expect(built_params[:bookmark][:shortening]).to eq "GDE"
+        end
+
+        it "should create new site" do
+          expect(built_params[:bookmark][:site_id]).to eq Site.last.id
+        end
+
+        it "should return params with no errors" do
+          expect(built_params[:errors].count).to eq 0
+        end
+      end
+    end
+  end
+end
