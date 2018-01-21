36a7cf119a775d29837b976beb367ed6b5e85004
lucek <lukasz.odziewa@gmail.com>
2018-01-21 21:36:09 +0100

    dependent destroy on tag and taggings

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ApplicationRecord
   validates_uniqueness_of :name
 
   belongs_to :user
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :bookmarks, through: :taggings
 end
