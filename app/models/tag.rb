c27c37bf58a86a034d6741427d44a0946cc69e11
lucek <lukasz.odziewa@gmail.com>
2018-01-21 17:37:06 +0100

    added taggings association to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ApplicationRecord
   validates_presence_of :name, :user_id
   validates_uniqueness_of :name
-  
+
   belongs_to :user
+  has_many :taggings
+  has_many :bookmarks, through: :taggings
 end
